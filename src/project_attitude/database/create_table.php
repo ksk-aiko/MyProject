e25845b943bd4d8038591b051d3a3b3656b4528a
ksk-aiko <step.up.to.brighter03@gmail.com>
2021-06-02 20:18:09 +0900

    deploy and create new table

--- src/project_attitude/database/create_table.php
+++ src/project_attitude/database/create_table.php
@@ -2,30 +2,41 @@
 
 require_once('mysqli.php');
 
+function dropTable($link)
+{
+    $dropTable = 'DROP TABLE IF EXISTS attitudes;';
+    $results = mysqli_query($link, $dropTable);
+    if ($results) {
+        echo 'テーブルを削除しました' . PHP_EOL;
+    } else {
+        echo 'Error: テーブルを削除できませんでした' . PHP_EOL;
+        echo 'Debugging error: ' . mysqli_error($link) . PHP_EOL;
+    }
+}
+
 function createTable($link)
 {
     $createTable = <<<EOT
-    DROP TABLE IF EXISTS tests;
-    CREATE TABLE tests (
+    CREATE TABLE attitudes (
         id INT NOT NULL AUTO_INCREMENT,
-        content VARCHAR(255),
+        content VARCHAR(100),
         PRIMARY KEY (id)
-    );
+      );
     EOT;
 
     $results = mysqli_query($link, $createTable);
     if ($results) {
         echo 'テーブルを作成しました' . PHP_EOL;
     } else {
-        echo '【Error】テーブルを作成できませんでした' . PHP_EOL;
-        echo '【Debugging error】: ' . mysqli_error($link) . PHP_EOL;
+        echo 'Error: テーブルを作成できませんでした' . PHP_EOL;
+        echo 'Debugging error: ' . mysqli_error($link) . PHP_EOL;
     }
 }
 
 
 
+
 $link = dbConnect();
+dropTable($link);
 createTable($link);
 mysqli_close($link);
-
-?>
