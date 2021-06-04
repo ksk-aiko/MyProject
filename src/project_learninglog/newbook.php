4f930384233ac557927912f3f0d09e0fb7435355
ksk-aiko <step.up.to.brighter03@gmail.com>
2021-06-04 12:24:44 +0900

    start learninglog project

---
+++ src/project_learninglog/newbook.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ラーニングログ</title>
+</head>
+<body>
+    <h1>ラーニングログ</h1>
+    <form action="table.php" method="post">
+        <input type="text" name="title">
+        <button>書籍を追加</button>
+    </form>
+</body>
+</html>
