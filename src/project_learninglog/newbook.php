a63942cb53a39c845810f399360364ef82d82790
ksk-aiko <step.up.to.brighter03@gmail.com>
2021-06-08 07:04:28 +0900

    apply card-style to learninglog

--- src/project_learninglog/newbook.php
+++ src/project_learninglog/newbook.php
@@ -1,16 +1,23 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ラーニングログ</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="stylesheets/style.css">
 </head>
+
 <body>
-    <h1>ラーニングログ</h1>
-    <form action="table.php" method="post">
-        <input type="text" name="title">
-        <button>書籍を追加</button>
-    </form>
+    <div class="container">
+        <h1>ラーニングログ</h1>
+        <form action="table.php" method="post" class="form-group">
+            <input type="text" name="title" class="form-control" placeholder="こちらに書籍名を入力してください">
+            <button class="btn btn-primary">書籍を追加</button>
+        </form>
+    </div>
 </body>
+
 </html>
--- src/project_learninglog/table.php
+++ src/project_learninglog/table.php
@@ -1,3 +1,32 @@
 <?php
 
-echo $_POST["title"];
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ラーニングログ</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="stylesheets/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+                <h2 class="card-title"><?php echo $_POST['title']; ?></h2>
+                <p class="card-text">
+                書籍が追加されました<br>
+                これからラーニングログアプリを<br>
+                作り込んでいきましょう！
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
