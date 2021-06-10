857c6a3548a56f1a6f229e12f498748d3d82d98c
ksk-aiko <step.up.to.brighter03@gmail.com>
2021-06-11 07:05:12 +0900

    start project basics

---
+++ src/project_basics/index.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>基礎を鍛えるアプリ</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="./stylesheets/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="header-left">基礎を鍛えるアプリ</div>
+            <div class="header-right">
+                <ul>
+                    <li>はじめに</li>
+                    <li>目的</li>
+                    <li class="selected">戦略</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
