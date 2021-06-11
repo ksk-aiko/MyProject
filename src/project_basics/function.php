54539c79e85cbccfa22be23e884d38ecc561c731
ksk-aiko <step.up.to.brighter03@gmail.com>
2021-06-11 20:02:05 +0900

    project_basic create function page

---
+++ src/project_basics/function.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>基礎を鍛えるアプリ</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  <link rel="stylesheet" href="./stylesheets/style.css">
+</head>
+
+<body>
+<div class="container">
+    <div class="list-group">
+    <a href="https://www.php.net/manual/en/function.sprintf.php" class="list-group-item list-group-item-action ">sprintf():フォーマットを指定して文字列を作る</a>
+    <a href="https://www.php.net/manual/en/function.strlen.php" class="list-group-item list-group-item-action ">strlen():文字数を表示する</a>
+    <a href="https://www.php.net/manual/en/function.trim.php" class="list-group-item list-group-item-action">trim():前後の空白や改行を除去する</a>
+    </div>
+</div>
+</body>
+
+</html>
--- src/project_basics/index.php
+++ src/project_basics/index.php
@@ -5,27 +5,59 @@
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>基礎を鍛えるアプリ</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <link rel="stylesheet" href="./stylesheets/style.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>基礎を鍛えるアプリ</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  <link rel="stylesheet" href="./stylesheets/style.css">
 </head>
 
 <body>
-    <div class="container">
-        <div class="header">
-            <div class="header-left">基礎を鍛えるアプリ</div>
-            <div class="header-right">
-                <ul>
-                    <li>はじめに</li>
-                    <li>目的</li>
-                    <li class="selected">戦略</li>
-                </ul>
-            </div>
-        </div>
+  <div class="container">
+    <div class="header">
+      <div class="header-left">基礎を鍛えるアプリ</div>
+      <div class="header-right">
+        <ul>
+          <li>はじめに</li>
+          <li>目的</li>
+          <li class="selected">戦略</li>
+        </ul>
+      </div>
     </div>
+    <div class="main">
+      <div class="contact-form">
+        <div class="form-title">お聞きします</div>
+        <form action="sent.php" method="post" class="form-group">
+          <label class="form-label" for="purpose">あなたの目的は何ですか？</label>
+          <input type="text" name="purpose" id="purpose" class="form-control">
+          <label class="form-label" for="strategy">戦略をお聞かせください</label>
+          <textarea name="strategy" class="form-control" id="strategy"></textarea>
+          <label for="age" class="form-label">あなたの年齢をお聞かせください</label><br>
+          <select name="age" id="age">
+            <option value="未選択">未選択</option>
+            <?php for ($i=6; $i<=100; $i++): ?>
+              <?php echo " <option value='{$i}歳'>{$i}歳</option> "; ?>
+            <?php endfor ?>
+          </select><br>
+          <input type="submit" value="送信" class="btn btn-primary">
+        </form>
+      </div>
+      <div class="introduction">
+      こちらのページではよく使う関数を紹介しています
+      </div>
+      <div class="transmit"><a href="function.php">こちらをクリック</a></div>
+    </div>
+    <div class="footer">
+      <div class="footer-left">
+        <ul>
+          <li>はじめに</li>
+          <li>目的</li>
+          <li class="selected">戦略</li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </body>
 
 </html>
