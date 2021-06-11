247cfe08e7e82d0bd87b6a101e99a07220c36ae2
ksk-aiko <step.up.to.brighter03@gmail.com>
2021-06-12 06:57:21 +0900

    add functions

--- src/project_basics/function.php
+++ src/project_basics/function.php
@@ -16,13 +16,16 @@
 </head>
 
 <body>
-<div class="container">
+  <div class="container">
     <div class="list-group">
-    <a href="https://www.php.net/manual/en/function.sprintf.php" class="list-group-item list-group-item-action ">sprintf():フォーマットを指定して文字列を作る</a>
-    <a href="https://www.php.net/manual/en/function.strlen.php" class="list-group-item list-group-item-action ">strlen():文字数を表示する</a>
-    <a href="https://www.php.net/manual/en/function.trim.php" class="list-group-item list-group-item-action">trim():前後の空白や改行を除去する</a>
+      <a href="https://www.php.net/manual/en/function.sprintf.php" class="list-group-item list-group-item-action ">sprintf():フォーマットを指定して文字列を作る</a>
+      <a href="https://www.php.net/manual/en/function.strlen.php" class="list-group-item list-group-item-action ">strlen():文字数を表示する</a>
+      <a href="https://www.php.net/manual/en/function.trim.php" class="list-group-item list-group-item-action">trim():前後の空白や改行を除去する</a>
+      <a href="https://www.php.net/manual/en/function.strpos.php" class="list-group-item list-group-item-action">strpos():文字列内を検索する</a>
+      <a href="https://www.php.net/manual/en/function.str-replace.php" class="list-group-item list-group-item-action">str_replace():文字列内の文字を置換する</a>
+      <a href="https://www.php.net/manual/en/function.mb-strlen.php" class="list-group-item list-group-item-action">mb_strlen():日本語の文字数をカウントする</a>
     </div>
-</div>
+  </div>
 </body>
 
 </html>
