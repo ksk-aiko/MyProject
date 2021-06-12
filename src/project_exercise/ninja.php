9f7bca2731a60cd3cb79e9a86ec4fe721257adb4
ksk-aiko <step.up.to.brighter03@gmail.com>
2021-06-13 07:06:48 +0900

    latest

--- src/project_exercise/ninja.php
+++ src/project_exercise/ninja.php
@@ -1,18 +1,4 @@
 <?php
-
-$list = [
-    1 => '忍者',
-    2 => ['CO', 'DE'],
-    3 => '{"text1":"無", "text2":"料"}',
-    4 => [
-      401 => '集',
-      402 => '人参',
-      403 => '問題',
-    ],
-  ];
-
-  $word1 = $list[1];
-  $word2 = implode($list[2]);
-  $word3 = json_decode($list[3])->text1 . json_decode($list[3])->text2;
-  $word4 = $list[4][403] . $list[4][401];
-  echo $word1 . $word2 . $word3 . $word4 . PHP_EOL;
+echo 1;
+echo 2;
+echo PHP_VERSION . PHP_EOL;
