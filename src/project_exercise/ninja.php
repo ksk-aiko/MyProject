ef6b71b9dbc66259af99d66f42b42ee41659ab60
ksk-aiko <step.up.to.brighter03@gmail.com>
2021-06-17 07:05:59 +0900

    update ninja.php

--- src/project_exercise/ninja.php
+++ src/project_exercise/ninja.php
@@ -1,11 +1,6 @@
 <?php
 
-$list = [-1, 0, 1, "2", "3A", 4.1, [5], "1,000", 1001];
-//配列から整数のみ取り出した配列を作成
-
-//foreachで1要素ずつ出力
-foreach ($list as $number) {
-    if (is_numeric($number)) {
-        echo $number . nl2br(PHP_EOL);
-    }
-}
+$num1 = 1;
+$num2 = 512;
+$answer = $num1 + $num2 / 1024;
+echo $answer . 'GB' . PHP_EOL;
