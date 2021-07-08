6a4812b6221630a5b2ef10568dc498cc84a755d2
ksk-aiko <step.up.to.brighter03@gmail.com>
2021-07-08 09:00:06 +0900

    create ninja9.php

--- src/project_exercise/intermediate/ninja9.php
+++ src/project_exercise/intermediate/ninja9.php
@@ -1,5 +1,18 @@
 <?php
 
 $url = 'https://example.com/a/b/c.php?d=1&e=2#f';
+// $component = parse_url($url, PHP_URL_QUERY);
+// $chars = str_split($component);
+// echo "{$chars[0]}:{$chars[2]}" . PHP_EOL;
+// echo "{$chars[4]}:{$chars[6]}" . PHP_EOL;
 
-
+$p = parse_url($url);
+var_dump($p);
+$q = explode("&", $p['query']);
+var_dump($q);
+foreach ($q as $s) {
+    $h = explode("=", $s);
+    if (count($h) === 2) {
+        echo "{$h[0]}:{$h[1]}" . PHP_EOL;
+    }
+}
