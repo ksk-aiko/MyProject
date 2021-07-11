4dc9505788555914cae9068ea22f771854c5b0d5
ksk-aiko <step.up.to.brighter03@gmail.com>
2021-07-11 23:17:30 +0900

    see and understand the answer in ninja10.php

--- src/project_exercise/intermediate/ninja10.php
+++ src/project_exercise/intermediate/ninja10.php
@@ -1,7 +1,14 @@
 <?php
 
 $path = '/var/www/html/example.com/index.html';
-$disassembledPath = explode('/', $path);
-array_splice($disassembledPath, 3, 0, 'test');
-$result = implode('/', $disassembledPath);
-echo $result . PHP_EOL;
+// $disassembledPath = explode('/', $path);
+// array_splice($disassembledPath, 3, 0, 'test');
+// $result = implode('/', $disassembledPath);
+// echo $result . PHP_EOL;
+
+$dir = dirname($path);
+$arr = explode('/', $dir);
+array_splice($arr, 3, 0, 'test');
+$dir = implode('/', $arr);
+$path = $dir . '/' . basename($path);
+echo $path . PHP_EOL;
