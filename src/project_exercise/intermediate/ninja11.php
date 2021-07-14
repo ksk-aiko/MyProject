5716c62cca3626e88e73afc44f0758bc8dc07ff3
ksk-aiko <step.up.to.brighter03@gmail.com>
2021-07-14 22:39:42 +0900

    create ninja11.php

--- src/project_exercise/intermediate/ninja11.php
+++ src/project_exercise/intermediate/ninja11.php
@@ -1,7 +1,17 @@
 <?php
-
+/*
+--------------------【自分で書いたコード】-------------------------------
 $html = '<img src="a.jpg" /><img src="b.jpg" /><img src="c.jpg" />';
 preg_match_all('/\w\.jpg/', $html, $matches);
 for ($i=0; $i<3; $i++) {
     echo $matches[0][$i] . PHP_EOL;
+}
+-------------------------------------------------------------------------
+*/
+
+$html = '<img src="a.jpg" /><img src="b.jpg" /><img src="c.jpg" />';
+if (preg_match_all('/<img src=\"(.+?)\" \/ >/', $html, $m)) {
+    foreach ($m[1] as $s) {
+        echo $s . PHP_EOL;
+    }
 }
